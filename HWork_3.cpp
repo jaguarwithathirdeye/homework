c26dde6e79a9d0f80445a14c159b2afe29626d8b
jaguarwithathirdeye <93216658+jaguarwithathirdeye@users.noreply.github.com>
2021-11-18 20:41:49 +0300

    Add files via upload

---
+++ HWork_3.cpp
@@ -0,0 +1,36 @@
+﻿
+//1
+#include <iostream>
+
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+float result;
+
+int main()
+{
+	result = a * (b + (float(c) / d));
+
+	std::cout << result;
+
+	return 0;
+
+}
+
+
+
+//2
+
+int main()
+{
+	int a = 33, b;
+
+	b = (a <= 21) ? 21 - a : (a - 21) * 2;
+
+	std::cout << b;
+
+	return 0;
+
+}
+
